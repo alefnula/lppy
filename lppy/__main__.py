3d408f460f907fe312f8571560d379c3a652753a
Viktor Kerkez <alefnula@gmail.com>
2020-08-06 00:10:41 +0100

    Add simple example.

--- lppy/__main__.py
+++ lppy/__main__.py
@@ -1,9 +1,65 @@
-from lppy.version import version
+import io
+import sys
+import json
+import time
+import argparse
+import subprocess
+
+import lppy
 
 
-def cli():
-    print(f"lppy: version={version}")
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("layout", type=str)
+    return parser
+
+
+class Server:
+    def __init__(self, lp: lppy.LaunchpadMiniMk3, layout: dict):
+        self.lp = lp
+
+        self.registry = {}
+        for item in layout:
+            button = item["button"]
+            color = lppy.RGB(**item["color"])
+            lp.led_on(color=color, n=button)
+            self.registry[item["button"]] = {
+                "action": item["action"],
+                "examples": item["examples"],
+            }
+        self.lp.input.set_callback(self.callback)
+
+    def callback(self, msg):
+        if msg[0][0] == 144 and msg[0][2] == 127:
+            button = msg[0][1]
+            if button in self.registry:
+                action = self.registry[button]["action"]
+                data = self.registry[button]["examples"]
+
+                if action == "script":
+                    exec(data)
+                elif action == "command":
+                    subprocess.call(data)
+
+    def run(self):
+        while True:
+            try:
+                time.sleep(100)
+            except KeyboardInterrupt:
+                return
+
+
+def cli(args):
+    parser = create_parser()
+    ns = parser.parse_args(args)
+    with io.open(ns.layout, "r") as f:
+        layout = json.loads(f.read())
+
+    lp = lppy.LaunchpadMiniMk3()
+    server = Server(lp=lp, layout=layout)
+
+    server.run()
 
 
 if __name__ == "__main__":
-    cli()
+    cli(sys.argv[1:])
